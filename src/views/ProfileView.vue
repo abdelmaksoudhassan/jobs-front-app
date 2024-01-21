0e014519324ba2f47de66b0b5148847bf5427367
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2024-01-21 17:00:40 +0200

    Update ProfileView.vue

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -117,7 +117,7 @@ export default{
             this.$router.back()
         },
         downloadCV(cv){
-            const url = 'https://nodejs-jobs.onrender.com'
+            const url = 'https://nodejs-jobs-5qdy.onrender.com'
             window.open(`${url}/${cv}`, "_blank");
         },
         onComplete(){
